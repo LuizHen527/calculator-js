c64794b0ee3fd1e41bcc93c5b8d62e5f987a3556
Luiz Henrique <luiz.henrique31415@gmail.com>
2025-12-10 16:16:29 -0300

    Add functions structure

---
+++ script.js
@@ -0,0 +1,33 @@
+function add(firstNum, secondNum) {
+    
+}
+
+function subtract(firstNum, secondNum) {
+    
+}
+
+function multiply(firstNum, secondNum) {
+    
+}
+
+function divide(firstNum, secondNum) {
+    
+}
+
+function operate(operator, firstNum, secondNum) {
+    if (operator === "+") {
+        add(firstNum, secondNum);
+
+    } else if (operator === "-") {
+        subtract(firstNum, secondNum);
+
+    } else if (operator === "*") {
+        subtract(firstNum, secondNum);
+
+    } else if (operator === "/") {
+        divide(firstNum, secondNum);
+        
+    }
+
+    
+}
