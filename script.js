ed805a201a04e4b94ae27db43ca954d813638ce8
Luiz Henrique <luiz.henrique31415@gmail.com>
2025-12-11 11:25:55 -0300

    Add function to call operation

--- script.js
+++ script.js
@@ -1,49 +1,91 @@
-function add(firstNum, secondNum) {
+function add(firstNum, ...secondNum) {
+    console.log("add");
     
 }
 
-function subtract(firstNum, secondNum) {
+function subtract(firstNum, ...secondNum) {
+    console.log("subtract");
     
 }
 
-function multiply(firstNum, secondNum) {
+function multiply(firstNum, ...secondNum) {
+    console.log("multiply");
     
 }
 
-function divide(firstNum, secondNum) {
+function divide(firstNum, ...secondNum) {
+    console.log("divide");
     
 }
 
-function operate(operator, firstNum, secondNum) {
-    if (operator === "+") {
-        add(firstNum, secondNum);
+function operate(numArr) {
+    let numberOne = null;
+    let numberTwo = null;
+    let operator = null;
+    let OPERATORS = "+-*/";    
 
-    } else if (operator === "-") {
-        subtract(firstNum, secondNum);
+    for (let digit of numArr) {
+    
+        if (OPERATORS.includes(digit)) {
+            operator = digit;
+            continue;
+            
+        } else {
+            if (numberOne === null) {
+                numberOne = digit;
+                continue;
 
-    } else if (operator === "*") {
-        subtract(firstNum, secondNum);
+            } else if (numberTwo === null) {
+                numberTwo = digit;
 
-    } else if (operator === "/") {
-        divide(firstNum, secondNum);
+            }
+        }
+
         
+
+        if (operator === "+") {
+            add(numberOne, numberTwo);
+
+        } else if (operator === "-") {
+            subtract(numberOne, numberTwo);
+
+        } else if (operator === "*") {
+            multiply(numberOne, numberTwo);
+
+        } else if (operator === "/") {
+            divide(numberOne, numberTwo);
+            
+        }
+
+
     }
-
-
 }
 
-function updateDisplay(digit) {
+function extractCalc(string) {
+    return string.split(" ");
     
 }
 
 function program() {
-    
+
     let numberButtons = document.querySelectorAll(".number-button");
-    let display = document.querySelector(".display");
+    let operatorButtons = document.querySelectorAll(".operator-button");
+    let resultButton = document.querySelector(".result-button");
+    let display = document.querySelector(".display span");
 
     numberButtons.forEach(item => item.addEventListener("click", () => {        
         display.textContent = display.textContent + item.textContent;
-    }))
+    }));
+
+    operatorButtons.forEach(item => item.addEventListener("click", () => {
+        display.textContent = display.textContent + " " + item.textContent + " ";
+    }));
+
+    resultButton.addEventListener("click", () => {
+        let calcArr = extractCalc(display.textContent);
+
+        operate(calcArr);
+    });
 }
 
 program();
